ecdb8707828c342cf64d7ca92a7087d8ef7fd3c9
lee yoon jin <mngvmn0707@gmail.com>
2023-06-21 01:20:46 +0900

    Truthy & Faisy

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-// 형 변환(Type Conversion)
+// 참과 거짓(Truthy & Faisy)
 
-const a = true;
-const b = 1;
+if (123) {
+  console.log("참!");
+}
 
-console.log(a === b);
+const fruits = ["Apple", "Banana"];
+const fruits2 = ['Apple'];
+
+if (fruits) {
+  console.log("fruits : 이 아이템이 들어있음!");
+}
+if (fruits2.length) {
+  console.log("fruits2 : 이 아이템이 들어있음!");
+} else {
+  console.log("fruits2 : 이 아이템은 없음");
+}
