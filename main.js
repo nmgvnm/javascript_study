ff528318f22d09aeb24422e20a405a5ff303d9c3
lee yoon jin <mngvmn0707@gmail.com>
2023-06-20 19:58:01 +0900

    boolean, null, undefined

--- main.js
+++
@@ -1,6 +0,0 @@
-const a = 0.1;
-const b = 0.2;
-
-console.log(typeof a);
-console.log(typeof (a + b).toFixed(1));
-console.log(Number((a + b).toFixed(1))); // 부동소수점 오류
---
+++ study-data/2-1.Ex.js
@@ -0,0 +1,10 @@
+const user = {
+  name: "yoonjin",
+  age: 85,
+  email : null,
+};
+
+console.log(user.name);
+console.log(user.age);
+console.log(user.email);
+console.log(user.abc);
