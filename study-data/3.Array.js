1657e146d0bd5624d53dc382553a235dd2efa4ff
lee yoon jin <mngvmn0707@gmail.com>
2023-06-20 23:34:35 +0900

    Array

---
+++ study-data/3.Array.js
@@ -0,0 +1,12 @@
+// 참조형 - Array
+const { last } = require("lodash");
+
+const fruits = ["Apple", "Banana", "Cherry"];
+
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[0]);
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[fruits.length-1]);
+
